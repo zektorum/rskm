51db199b78ade0316592c8ffbc0512047d070f39
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2026-03-16 21:45:56 +0300

    implement ssh_keygen_args, fix typo in ECDSASK key name

--- src/keys/keys.rs
+++ src/keys/keys.rs
@@ -4,23 +4,36 @@ enum KeyTypes {
     ED25519,
     ED25519SK,
     ECDSA,
-    ECSASK,
+    ECDSASK,
     XMSS,
-    RSA
+    RSA,
 }
 
 impl FromStr for KeyTypes {
     type Err = String;
-    
+
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_uppercase().as_str() {
-            "ED25519" => Ok(KeyTypes::ED25519),
+            "ED25519"   => Ok(KeyTypes::ED25519),
             "ED25519SK" => Ok(KeyTypes::ED25519SK),
-            "ECDSA" => Ok(KeyTypes::ECDSA),
-            "ECSASK" => Ok(KeyTypes::ECSASK),
-            "XMSS" => Ok(KeyTypes::XMSS),
-            "RSA" => Ok(KeyTypes::RSA),
-            _ => Err(format!("Unknown key type: {}", s))
+            "ECDSA"     => Ok(KeyTypes::ECDSA),
+            "ECDSASK"   => Ok(KeyTypes::ECDSASK),
+            "XMSS"      => Ok(KeyTypes::XMSS),
+            "RSA"       => Ok(KeyTypes::RSA),
+            _           => Err(format!("Unknown key type: {}", s)),
+        }
+    }
+}
+
+impl KeyTypes {
+    fn ssh_keygen_args(&self) -> Vec<&str> {
+        match self {
+            KeyTypes::ED25519   => vec!["-t", "ed25519"],
+            KeyTypes::ED25519SK => vec!["-t", "ed25519-sk"],
+            KeyTypes::ECDSA     => vec!["-t", "ecdsa", "-b", "521"],
+            KeyTypes::ECDSASK   => vec!["-t", "ecdsa-sk"],
+            KeyTypes::XMSS      => vec!["-t", "xmss"],
+            KeyTypes::RSA       => vec!["-t", "rsa", "-b", "4096"],
         }
     }
 }
