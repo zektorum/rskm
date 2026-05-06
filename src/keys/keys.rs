956da64dc2d8bb8ea3d1c7b01e91f8a4ebf85d9f
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2026-05-06 20:56:32 +0300

    delete everything except KeyTypes from keys.rs

--- src/keys/keys.rs
+++ src/keys/keys.rs
@@ -1,8 +1,4 @@
-use std::path::{Path, PathBuf};
-use std::process::Command;
 use std::str::FromStr;
-
-use dirs::home_dir;
 
 pub enum KeyTypes {
     ED25519,
@@ -21,37 +17,6 @@ impl FromStr for KeyTypes {
             "XMSS"      => Ok(KeyTypes::XMSS),
             "RSA"       => Ok(KeyTypes::RSA),
             _           => Err(format!("Unknown key type: {}", s)),
-        }
-    }
-}
-
-impl KeyTypes {
-    // Default ssh-keygen args for given key type
-    fn ssh_keygen_args(&self) -> Vec<&str> {
-        match self {
-            KeyTypes::ED25519   => vec!["-t", "ed25519"],
-            KeyTypes::ECDSA     => vec!["-t", "ecdsa", "-b", "521"],
-            KeyTypes::XMSS      => vec!["-t", "xmss"],
-            KeyTypes::RSA       => vec!["-t", "rsa", "-b", "4096"],
-        }
-    }
-}
-
-pub struct KeyGenSettings {
-    key_type: KeyTypes,
-    no_passphrase: bool,
-    output_dir: PathBuf
-}
-
-impl Default for KeyGenSettings {
-    fn default() -> Self {
-        let output_dir = home_dir()
-            .unwrap()
-            .join(".rskm");
-        KeyGenSettings {  
-            key_type: KeyTypes::ED25519,
-            no_passphrase: true,
-            output_dir
         }
     }
 }
