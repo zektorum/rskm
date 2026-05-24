bbaa7692be3ae38c22e263e3bfbe92f533837497
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2026-05-24 23:29:37 +0300

    style: rename KeyTypes variants to PascalCase

--- src/keys/key_types.rs
+++ src/keys/key_types.rs
@@ -1,10 +1,10 @@
 use std::str::FromStr;
 
 pub enum KeyTypes {
-    ED25519,
-    ECDSA,
-    XMSS,
-    RSA,
+    Ed25519,
+    Ecdsa,
+    Xmss,
+    Rsa,
 }
 
 impl FromStr for KeyTypes {
@@ -12,10 +12,10 @@ impl FromStr for KeyTypes {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_uppercase().as_str() {
-            "ed25519"   => Ok(KeyTypes::ED25519),
-            "ecdsa"     => Ok(KeyTypes::ECDSA),
-            "xmss"      => Ok(KeyTypes::XMSS), //TODO: make feature-toggle for xmss support
-            "rsa"       => Ok(KeyTypes::RSA),
+            "ed25519"   => Ok(KeyTypes::Ed25519),
+            "ecdsa"     => Ok(KeyTypes::Ecdsa),
+            "xmss"      => Ok(KeyTypes::Xmss), //TODO: make feature-toggle for xmss support
+            "rsa"       => Ok(KeyTypes::Rsa),
             _           => Err(format!("Unknown key type: {}", s)),
         }
     }
